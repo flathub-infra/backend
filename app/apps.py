8ae8cb3098d6dadd8b48cd63a317b7fbd2258517
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2020-11-30 11:16:43 +0100

    apps: Use actual icon name from app-info
    Appstream is a gift that keeps on giving. appstream-compose sets the
    icon name based on the value of <launchable> tag and not the name or ID
    used by the referenced desktop file. The assumption that appid == icon
    filename is wrong, even if happens to be true most of the time.
    
    See https://github.com/flathub/flathub/pull/1949 for details.

--- app/apps.py
+++ app/apps.py
@@ -47,8 +47,10 @@ def get_icon_path(app):
         if cached_icons:
             cached_icons.sort()
             size = cached_icons[0]
+            name = icons[0]["value"]
+
             icon_path = (
-                f"{cdn_baseurl}/repo/appstream/x86_64/icons/{size}x{size}/{appid}.png"
+                f"{cdn_baseurl}/repo/appstream/x86_64/icons/{size}x{size}/{name}"
             )
             return icon_path
 
@@ -369,7 +371,6 @@ def search(userquery: str):
 
     # redisearch does not support fuzzy search for non-alphabet strings
     if userquery.isalpha():
-    if userquery.isalnum():
         generic_query = redisearch.Query(f"%{userquery}%").no_content()
     else:
         generic_query = redisearch.Query(userquery).no_content()
